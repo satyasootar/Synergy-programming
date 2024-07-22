b37634eea7fb92f09ef094c3cf0f3d090bf7649d
Satya Sootar <satya.sootar06@gmail.com>
2024-07-22 16:34:12 +0530

    added java

---
+++ Java/VolumeCuboid.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class VolumeCuboid{
+  public static void main(String args[]){
+  
+  int l = 4, b = 5, h = 7, vol;
+  vol = l*b*h;
+  
+  System.out.print("The Volume of the cuboid is = "+vol);
+  }
+}
