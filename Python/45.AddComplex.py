8e5176b8194be20aa5a322048c1d9aab4b0a46be
satyasootar <satya.sootar06@gmail.com>
2024-10-01 12:07:08 +0530

    complex

--- Python/45.AddComplex.py
+++ Python/45.AddComplex.py
@@ -1,3 +1,5 @@
+#WAP to add two complex number using class
+
 class ComplexNumber:
     def __init__(self, real, imag):
         self.real = real
@@ -12,20 +14,26 @@ class ComplexNumber:
         print(f"{self.real} + {self.imag}i")
 
 
-# Input two complex numbers
 real1 = float(input("Enter the real part of the first complex number: "))
 imag1 = float(input("Enter the imaginary part of the first complex number: "))
 
 real2 = float(input("Enter the real part of the second complex number: "))
 imag2 = float(input("Enter the imaginary part of the second complex number: "))
 
-# Create complex number objects
+
 num1 = ComplexNumber(real1, imag1)
 num2 = ComplexNumber(real2, imag2)
 
-# Add the two complex numbers
+
 result = num1.add(num2)
 
-# Display the result
 print("The sum of the complex numbers is:")
 result.display()
+
+# Output 
+# Enter the real part of the first complex number: 4
+# Enter the imaginary part of the first complex number: 21
+# Enter the real part of the second complex number: 32
+# Enter the imaginary part of the second complex number: 123
+# The sum of the complex numbers is:
+# 36.0 + 144.0i
