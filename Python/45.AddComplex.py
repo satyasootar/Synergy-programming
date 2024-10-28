24e84894b84c1fd67f17e95b656cc7e79ab8a581
Satya Sootar <satya.sootar06@gmail.com>
2024-10-28 18:32:18 +0530

    Info for 46 added

--- Python/45.AddComplex.py
+++ Python/45.AddComplex.py
@@ -37,3 +37,6 @@ result.display()
 # Enter the imaginary part of the second complex number: 123
 # The sum of the complex numbers is:
 # 36.0 + 144.0i
+
+
+#  * Code number 46 is at the top of the repository. It's a Folder *
