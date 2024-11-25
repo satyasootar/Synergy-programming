30492c23adc8c4891c0d704d5042bca5e5d01dbf
satyasootar <satya.sootar06@gmail.com>
2024-11-25 15:46:58 +0530

    Applet added

---
+++ Java/Applet(25-11-24)/ABpplet.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class ABpplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello, Java Applet!", 20, 20);
+    }
+}
+
